03004d218a143e24e15291625f258a8626939304
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-09-22 17:51:01 +0800

    fixed user permissions error

--- petreon/users/permissions.py
+++ petreon/users/permissions.py
@@ -8,9 +8,9 @@ class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == request.user
+        return obj == request.user
 
-
+ 
 class IsSuperUserOrReadOnly(permissions.IsAdminUser):
     """
     For use with admin-level change views.
