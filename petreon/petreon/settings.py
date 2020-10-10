921cda030e5efe4da9f128d9401f49245a8cea3f
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-10-10 17:08:52 +0800

    session added back

--- petreon/petreon/settings.py
+++ petreon/petreon/settings.py
@@ -76,7 +76,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
